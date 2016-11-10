8efc62d20cf4bf58ea8e8312a040f471e7b15195
lombax5832 <iPodAlexei@gmail.com>
2016-11-10 12:00:53 -0500

    XMLParse start

--- XMLParse.h
+++ XMLParse.h
@@ -1,1 +1,17 @@
 #pragma once
+using namespace std;
+#include<string>
+#include<vector>
+
+struct Element {
+  string name;
+  string value;
+  vector<Element> children;
+};
+
+class XMLParse {
+public:
+
+private:
+
+};
