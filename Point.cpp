bb815dd048afda9efbfc09f172305ba7441b9f88
lombax5832 <iPodAlexei@gmail.com>
2016-11-10 12:00:40 -0500

    Point Implementation

---
+++ Point.cpp
@@ -0,0 +1,28 @@
+#include "Point.h"
+
+#include<cmath>
+
+Point::Point(double xCoord, double yCoord) {
+  setX(xCoord);
+  setY(yCoord);
+}
+
+double Point::distanceTo(const Point point) const {
+  return sqrt((getX() - point.getX())*(getX() - point.getX()) - (getY() - point.getY())*(getY() - point.getY()));
+}
+
+double Point::getX() const {
+  return x;
+}
+
+double Point::getY() const {
+  return y;
+}
+
+void Point::setX(double xCoord) {
+  x = xCoord;
+}
+
+void Point::setY(double yCoord) {
+  y = yCoord;
+}
--- Point.h
+++ Point.h
@@ -1,1 +1,15 @@
-#pragma once
+class Point {
+public:
+  Point(double xCoord = 0.0, double yCoord = 0.0);
+
+  double distanceTo(const Point point) const;
+
+  double getX() const;
+  double getY() const;
+
+  void setX(double xCoord);
+  void setY(double yCoord);
+private:
+  double x;
+  double y;
+};
